28e59eb61508a742a2d5f9b4eb8b20fe417c4266
jesus is king <fadi.b.alhabib@gmail.com>
2025-05-28 08:03:04 +0300

    homepage section-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,60 @@
 import { Box, Center, HStack, Image, Text } from "@chakra-ui/react";
+import { useAnimation } from "framer-motion";
+import { useEffect } from "react";
 import "./App.css";
-import { arrowDown, banner, logo } from "./core/constants/assets";
+import {
+  arrowDown,
+  banner,
+  blackAnimation,
+  logo,
+} from "./core/constants/assets";
 import { MotionBox, MotionText } from "./core/utils/motion-components";
 
 function App() {
+  const controls = useAnimation();
+  useEffect(() => {
+    async function sequence() {
+      await controls.start({
+        opacity: 1,
+        transition: { duration: 3, ease: "easeOut" },
+      });
+
+      await controls.start({
+        y: 0,
+        x: 0,
+        transition: { duration: 1.5, ease: "easeOut" },
+      });
+      await controls.start({
+        x: [-20, -18, -16, -14, 0],
+        textShadow: [
+          `
+    1px 1px 0 #ccc,
+    2px 2px 0 #bbb,
+    3px 3px 0 #aaa,
+    4px 4px 0 #999,
+    5px 5px 0 #888
+  `,
+          `0`,
+        ],
+        // skew: [0, 12, 10, 6, 0],
+        opacity: [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+        transition: {
+          duration: 1,
+          ease: "circIn",
+          times: [0, 0.15, 0.3, 0.45, 0.6, 0.8, 1],
+        },
+      });
+
+      await controls.start({
+        x: 0,
+        skewX: 0,
+        opacity: 1,
+        transition: { duration: 1, ease: "easeInOut" },
+      });
+    }
+
+    sequence();
+  }, [controls]);
   return (
     <Box>
       <Box position="relative" width="100%" height="100vh" overflow="hidden">
@@ -101,6 +152,50 @@ function App() {
             </MotionBox>
           </Center>
         </Box>
+      </Box>
+
+      <Box
+        height={"100vh"}
+        width={"100%"}
+        background={`url("${blackAnimation}")`}
+        backgroundRepeat={"no-repeat"}
+        backgroundSize={"contain"}
+        backgroundPosition={"center"}
+        textAlign={"center"}
+        display={"flex"}
+        flexDir={"column"}
+        justifyContent={"center"}
+        overflow={"hidden"}
+      >
+        <MotionText
+          initial={{ y: 75, x: 0, opacity: 0 }}
+          animate={controls}
+          style={{
+            fontSize: "3.7rem",
+            fontWeight: "700",
+            willChange: "transform, opacity",
+          }}
+        >
+          Morphios is a living practice.
+        </MotionText>
+        <MotionText
+          fontSize={"2rem"}
+          fontWeight={"400"}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2, delay: 3 }}
+        >
+          A whisper in performance
+        </MotionText>
+        <MotionText
+          fontSize={"2rem"}
+          fontWeight={"400"}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2, delay: 5 }}
+        >
+          A mirror in film
+        </MotionText>
       </Box>
     </Box>
   );
--- src/core/constants/assets.ts
+++ src/core/constants/assets.ts
@@ -1,5 +1,7 @@
 import banner from "../../assets/gif/banner.gif";
 import arrowDown from "../../assets/svg/arrowdown.svg";
+import blackAnimation from "../../assets/svg/black-animation.svg";
 import logo from "../../assets/svg/logo.svg";
 import underline from "../../assets/svg/underline.svg";
-export { arrowDown, banner, logo, underline };
+
+export { arrowDown, banner, blackAnimation, logo, underline };
